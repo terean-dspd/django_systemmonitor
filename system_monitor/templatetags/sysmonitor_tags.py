b370524da4f53cb66c5231141fba8f291fb3f2e3
Dennis <denniskot@gmail.com>
2017-09-04 23:14:10 +0300

    ver 1.0.1

--- system_monitor/templatetags/sysmonitor_tags.py
+++ system_monitor/templatetags/sysmonitor_tags.py
@@ -2,15 +2,17 @@ import psutil
 from django              import template
 from django.conf         import settings
 
-
+try:
+    path = settings.DISK_USAGE_PATH
+except:
+    path = "/"
 register = template.Library()
 @register.inclusion_tag('system_monitor/donut.html', takes_context=True)
 def disk_donut(context):
-    try:
-        path = settings.DISK_USAGE_PATH
-    except:
-        path = "/"
     disk_usage = psutil.disk_usage(path)
     return {
         'disk_usage_free':disk_usage.percent,
     }
+@register.simple_tag
+def disk_text():
+    return disk_usage = psutil.disk_usage(path)
